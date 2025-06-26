2f634fced57f79cf8b3d3a497ca0664b3b496dde
Linnea Gräf <nea@nea.moe>
2025-06-27 01:45:05 +0200

    chore: use tabs over spaces

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,10 +1,10 @@
 use twilight_model::user::User;
 
 pub trait UserExt {
-    fn mention(&self) -> String;
+	fn mention(&self) -> String;
 }
 impl UserExt for User {
-    fn mention(&self) -> String {
-        format!("<@{}>", self.id)
-    }
+	fn mention(&self) -> String {
+		format!("<@{}>", self.id)
+	}
 }
